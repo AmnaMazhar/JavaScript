c25f457a3cc2565061b8215cf75359a364498048
Amna Mazhar <76264527+AmnaMazhar@users.noreply.github.com>
2022-06-05 18:08:16 +0500

    correct and update task7
    correct and update task7 completely

--- Week3/task7.js
+++ Week3/task7.js
@@ -1,8 +1,28 @@
-// Create a parametrized function for 4,5 where the array is recieved as an input.
+// Create a parametrized function for task4, task5 where the array is recieved as an input.
 
-function param_func(array)
+// task 6
+function task4(array)
 {
-    console.log(array);
+for(let index = 0; index < array.length; index++)
+{
+    console.log("Task 4: "+array[index]);
+}
 }
 
-param_func([4,5]);
+// task 7
+function task5(array)
+{
+    let sum = 0;
+
+for (let index = 0; index < array.length; index++)
+{
+    num = array[index];
+    sum = sum + num;
+}
+
+console.log("Task 5: "+sum);
+}
+
+task4([2,6,10]);
+console.log("\n")
+task5([11,24,56]);
