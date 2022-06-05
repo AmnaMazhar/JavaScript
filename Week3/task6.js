975cc6b56c7c58b7e36942629b2d3853d051a5de
Amna Mazhar <76264527+AmnaMazhar@users.noreply.github.com>
2022-06-05 18:07:01 +0500

    correct and update task6
    correct and update task6 completely

--- Week3/task6.js
+++ Week3/task6.js
@@ -1,11 +1,40 @@
-// Create functions for 1,2,3
+// Create functions for task1, task2, task3
 
-function num()
+// task 1
+function task1()
 {
-    for (let i=1; i < 4; i++)
-    {
-        console.log(i);
-    }
+    for (let num = 1; num <= 10; num++)
+{
+    console.log("Task 1: "+num);
+    
+}
 }
 
-num()
+// task 2
+function task2()
+{
+    let num = 2;
+for (let num = 2; num <= 10; num = num+2)
+{
+    console.log("Task 2: "+num);
+}
+}
+
+// task 3
+function task3(){
+    for (let num = 2; num <= 10; num = num + 2)
+{
+    if (num === 6 || num === 8)
+    {
+        continue;
+    }
+    console.log("Task 3: "+num);
+}      
+
+}
+
+task1()
+console.log("\n")
+task2()
+console.log("\n")
+task3()
